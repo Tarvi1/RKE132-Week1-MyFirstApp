50ec5e438e506a0ae325c84bdd90dd1bd927eb4f
Tarvi Rem <tarvi.remmelgas@tktk.ee>
2024-03-09 15:54:56 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada tema nimi
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("enter your name:");
+//string - sõna
+String username;
+
+username = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + username);
+//string interpolation
+
+firstname = "Harry";
+lastname = "Potter";
+Console.WriteLine($"Hello, {username}!");
+Console.WriteLine("Hello, " + firstname + lastname);
